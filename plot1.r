850cbf271955ed0c0f199375db959139d4a89513
gapts <kanygina.a@CNU2171SFL-W7.na.pg.com>
2014-05-12 01:09:12 +0400

    -a

---
+++ plot1.r
@@ -0,0 +1,37 @@
+## Exploratory Data Analysis, Course Project #1
+
+#upload and unzip data
+fileUrl<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+td = tempdir()
+tf = tempfile(tmpdir=td, fileext=".zip")
+download.file(fileUrl, tf)
+flist = unzip(tf, list=TRUE)
+fname = unzip(tf, list=TRUE)$Name[1]
+unzip(tf, files=fname, exdir=td, overwrite=TRUE)
+fpath = file.path(td, fname)
+data<-read.csv(fpath,sep=";",stringsAsFactor=F,header=TRUE)
+#file<-basename(fpath)  
+#assign(paste(file[],sep=""),tmp)
+
+#Data transformation & clean-up
+data$Date<-as.Date(data$Date,format="%d/%m/%Y")
+data$dtime<-paste(data$Date,data$Time)
+data$dtime<-as.POSIXct(data$dtime,format="%Y-%m-%d %H:%M:%S")
+data$Global_active_power<-as.numeric(data$Global_active_power)
+data$Global_reactive_power<-as.numeric(data$Global_reactive_power)
+data$Voltage<-as.numeric(data$Voltage)
+data$Global_intensity<-as.numeric(data$Global_intensity)
+data$Sub_metering_1<-as.numeric(data$Sub_metering_1)
+data$Sub_metering_2<-as.numeric(data$Sub_metering_2)
+data$Sub_metering_3<-as.numeric(data$Sub_metering_3)
+data$check<-ifelse(data$Date == ("2007-02-01") | data$Date == ("2007-02-02"),"Y","-")
+
+#Subsetting needed data for the dates 2007-02-01 and 2007-02-02.
+dta2<-data[which(data$check=="Y"),]
+
+#plot1
+hist(dta2$Global_active_power, col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.copy(png, file = "plot1.png")
+dev.off()
+
+
---
+++ plot3.r
@@ -0,0 +1,41 @@
+## Exploratory Data Analysis, Course Project #1
+
+#upload and unzip data
+fileUrl<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+td = tempdir()
+tf = tempfile(tmpdir=td, fileext=".zip")
+download.file(fileUrl, tf)
+flist = unzip(tf, list=TRUE)
+fname = unzip(tf, list=TRUE)$Name[1]
+unzip(tf, files=fname, exdir=td, overwrite=TRUE)
+fpath = file.path(td, fname)
+data<-read.csv(fpath,sep=";",stringsAsFactor=F,header=TRUE)
+#file<-basename(fpath)  
+#assign(paste(file[],sep=""),tmp)
+
+#Data transformation & clean-up
+data$Date<-as.Date(data$Date,format="%d/%m/%Y")
+data$dtime<-paste(data$Date,data$Time)
+data$dtime<-as.POSIXct(data$dtime,format="%Y-%m-%d %H:%M:%S")
+data$Global_active_power<-as.numeric(data$Global_active_power)
+data$Global_reactive_power<-as.numeric(data$Global_reactive_power)
+data$Voltage<-as.numeric(data$Voltage)
+data$Global_intensity<-as.numeric(data$Global_intensity)
+data$Sub_metering_1<-as.numeric(data$Sub_metering_1)
+data$Sub_metering_2<-as.numeric(data$Sub_metering_2)
+data$Sub_metering_3<-as.numeric(data$Sub_metering_3)
+data$check<-ifelse(data$Date == ("2007-02-01") | data$Date == ("2007-02-02"),"Y","-")
+
+#Subsetting needed data for the dates 2007-02-01 and 2007-02-02.
+dta2<-data[which(data$check=="Y"),]
+
+#plot3
+plot(dta2$dtime,dta2$Sub_metering_1, type="n",xlab="",ylab="Energy sub metering") 
+lines(dta2$dtime,dta2$Sub_metering_1,)
+par()
+lines(dta2$dtime,dta2$Sub_metering_2,col="red")
+lines(dta2$dtime,dta2$Sub_metering_3,col="blue")
+legend("topright", pch = "-", col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"),cex=0.8)
+dev.copy(png, file = "plot3.png")
+dev.off()
+
